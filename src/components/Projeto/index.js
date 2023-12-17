58dadf0533ded42d769562fbe9d21d2a35b293d6
Yoko Lucila Takano <takano_yokolucila@outlook.com>
2023-12-16 22:38:44 -0400

    inicia a criação das tags

--- src/components/Projeto/index.js
+++ src/components/Projeto/index.js
@@ -2,6 +2,7 @@ import styles from "./Projeto.module.css";
 import Card from "components/Card";
 
 const Projeto = ({
+  id,
   name,
   startDate,
   endDate,
@@ -9,14 +10,15 @@ const Projeto = ({
   companyName,
   secondaryColor,
   members,
+  tags,
 }) => {
   const borderLine = { borderColor: secondaryColor };
   return (
     <section className={styles.formulario}>
       <form>
         <h3 style={borderLine}>{name}</h3>
-        <p>
-          {startDate} | {endDate}
+        <p className={styles.datas}>
+          {startDate} ~ {endDate}
         </p>
         <p>
           <strong>Description:</strong> {description}
@@ -25,6 +27,16 @@ const Projeto = ({
           <strong>Company Name: </strong>
           {companyName}
         </p>
+        <p>
+          <strong>Tags:</strong>
+        </p>
+        <div className={styles.tags}>
+          <ul>
+            {tags.map((tag, index) => (
+              <li key={index}>{tag}</li>
+            ))}
+          </ul>
+        </div>
         <div className={styles.colaborador}>
           <div className={styles.times}>
             {members.map((member) => (
