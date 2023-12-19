fdbad7d269f1f57c31b1abd3e044fefe0a69380f
Yoko Lucila Takano <takano_yokolucila@outlook.com>
2023-12-19 16:25:37 -0400

    acrescenta o uso do `JsonServer`

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,14 +1,25 @@
 import Projeto from "components/Projeto";
-import data from "json/db.json";
 import styles from "./Inicio.module.css";
 import { Fragment } from "react";
+import { useEffect, useState } from "react";
 
 const Inicio = () => {
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/yoko-takano/frontend-api/projects"
+    )
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setProjects(dados);
+      });
+  }, []);
+
   return (
     <Fragment>
       <br /> <br /> <br /> <br /> <br />
       <section className={styles.formulario}>
-        {data.map((project) => {
+        {projects.map((project) => {
           return <Projeto {...project} key={project.id} />;
         })}
       </section>
