a51e50c7d89c49046730051b266a6ebd21909069
Yoko Lucila Takano <takano_yokolucila@outlook.com>
2023-12-18 20:20:50 -0400

    retira o icone de youtube do rodape e exclui fotos não usadas

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -14,14 +14,6 @@ const Rodape = () => {
             </a>
           </li>
           <li>
-            <a
-              href="https://l.instagram.com/?u=https%3A%2F%2Fyoutube.com%2Flive%2FKyjaFChbQog%3Ffeature%3Dshare&e=AT1AsdOjMUjtmfsXVACQ5WbJF9nnpHYM6xPe8VA1Ayrm3PsltGTSws07rD3tyO_v-yUcSthjsr93xFbUMS23XnxVJw9-knj76e1ljAI9DGehZXpX5uSSQ4Q"
-              target="_blank"
-            >
-              <i className="fa-brands fa-youtube"></i>
-            </a>
-          </li>
-          <li>
             <a href="https://www.instagram.com/hubtecnologia/" target="_blank">
               <i className="fa-brands fa-instagram"></i>
             </a>
